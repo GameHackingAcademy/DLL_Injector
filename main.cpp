1cfc5ec41c272e719cbc3dc89d21b5c25bd29602
attilathedud <attilathedud@gmail.com>
2020-12-27 17:27:21 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,30 @@
+/* 
+A DLL injector that injects the DLL specified in dll_path into the Urban Terror process. This injector requires Urban Terror to be running. 
+
+A different process can be specified by changing the executable name in the strcmp call.
+
+To load static and dynamic libraries, Windows executables can use the LoadLibraryA API function. This function takes a single argument, 
+which is a full path to the library to load:
+
+HMODULE LoadLibraryA(
+    LPCSTR lpLibFileName
+);
+
+If we call LoadLibraryA in our injector's code, the DLL will be loaded into our injector's memory. Instead, we want our injector to force the game 
+to call LoadLibraryA. To do this, we will use the CreateRemoteThread API to create a new thread in the game. This thread will then execute LoadLibraryA 
+inside the game's running process.
+
+However, since the thread is running inside the game's memory, LoadLibraryA will not be able to find the path of our DLL specified in our injector.
+To get around this, we have to write our DLL's path into the game's memory. To ensure that we do not corrupt any other memory, we will also need to 
+allocate additional memory inside the game using VirtualAllocEx. 
+
+The full explanation for how this code works is available at https://gamehacking.academy/lesson/25
+*/
+
 #include <windows.h>
 #include <tlhelp32.h>
 
-const char* dll_path = "C:\\Users\\IEUser\\source\\repos\\wallhack\\Debug\\wallhack.dll";
+const char *dll_path = "C:\\Users\\IEUser\\source\\repos\\wallhack\\Debug\\wallhack.dll";
 
 int main(int argc, char** argv) {
 	HANDLE snapshot = 0;
